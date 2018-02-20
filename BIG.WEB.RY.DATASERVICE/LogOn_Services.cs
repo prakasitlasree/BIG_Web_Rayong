1c53b114b45b986a21b3c824774d456a34202c3a
https://github.com/prakasitlasree/part-search.git <prakasitlasree@gmail.com>
2018-02-20 22:46:50 +0700

    update ef

---
+++ BIG.WEB.RY.DATASERVICE/LogOn_Services.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+ 
+ 
+namespace BIG.WEB.RY.DATASERVICE
+{
+   public class LogOn_Services
+    {
+        //public List<LOGON> GetAll()
+        //{
+        //    var result = new List<LOGON>(); 
+        //    try
+        //    {
+        //        using (var ctx = new BIG_RY_DBEntities())
+        //        {
+
+                    
+        //        }
+        //        return result;
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        throw ex;
+        //    }
+        //}
+    }
+}
