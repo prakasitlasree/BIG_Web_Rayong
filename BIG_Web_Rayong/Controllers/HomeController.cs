50ab770d0611663942642542a4f039a9ea2f8e83
Jebaited <Jebaited@SIWADOLKUTTIYA>
2018-02-23 19:59:34 +0700

    add page

--- BIG_Web_Rayong/Controllers/HomeController.cs
+++ BIG_Web_Rayong/Controllers/HomeController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using BIG.WEB.RY.DATASERVICE;
+using BIG.WEB.RY.MODEL;
 
 namespace BIG_Web_Rayong.Controllers
 {
@@ -10,6 +12,8 @@ namespace BIG_Web_Rayong.Controllers
     {
         public ActionResult Index()
         {
+            LogOn_Services service = new LogOn_Services();
+            var x = service.GetAll();
             return View();
         }
 
