0179980d083eb05230c9b5c08e516f5ce9b2b423
Jebaited <Jebaited@SIWADOLKUTTIYA>
2018-03-01 20:42:07 +0700

    add all admin

--- BIG.WEB.RY.DATASERVICE/PageContent_Services.cs
+++ BIG.WEB.RY.DATASERVICE/PageContent_Services.cs
@@ -8,7 +8,7 @@ using BIG.WEB.RY.MODEL;
 
 namespace BIG.WEB.RY.DATASERVICE
 {
-   public class PageContent_Services
+    public class PageContent_Services
     {
         public List<PAGE_CONTENT> GetAll()
         {
@@ -27,5 +27,100 @@ namespace BIG.WEB.RY.DATASERVICE
                 throw ex;
             }
         }
+
+        public Result AddPageContent(PAGE_CONTENT dataInput)
+        {
+            try
+            {
+                var content = GetAll().Where(x => x.SECTION_NAME == dataInput.SECTION_NAME).ToList();
+
+                if (content.Count > 0)
+                {
+                    int maxId = content.Max(t => t.AUTO_ID);
+                    dataInput.AUTO_ID = maxId + 1;
+
+
+                }
+                else
+                {
+                    dataInput.AUTO_ID = 1;
+                }
+
+                using (var context = new BIG_RY_DBEntities())
+                {
+                    context.PAGE_CONTENT.Add(dataInput);
+                    context.SaveChanges();
+
+                    return new Result() { Message = "Success", Status = true };
+
+                }
+
+            }
+            catch (Exception ex)
+            {
+
+                return new Result() { Message = ex.Message.ToString(), Status = false };
+            }
+
+
+        }
+        public Result EditPageContent(PAGE_CONTENT dataInput)
+        {
+            try
+            {
+                using (var context = new BIG_RY_DBEntities())
+                {
+                    var update = context.PAGE_CONTENT.Where(x => x.AUTO_ID == dataInput.AUTO_ID).FirstOrDefault();
+                    if (update != null)
+                    {
+                        update.HTML_SUB_HEADER1 = dataInput.HTML_SUB_HEADER1;
+                        update.HTML_SUB_HEADER2 = dataInput.HTML_SUB_HEADER2;
+                        update.HTML_VALUE = dataInput.HTML_VALUE;
+                        update.IMAGE_URL = dataInput.IMAGE_URL == null ? update.IMAGE_URL : dataInput.IMAGE_URL;
+                        update.STATUS = dataInput.STATUS;
+                        update.SEQ = dataInput.SEQ;
+
+                    }
+                    context.SaveChanges();
+
+                    return new Result() { Message = "Success", Status = true };
+
+                }
+
+            }
+            catch (Exception ex)
+            {
+                return new Result() { Message = ex.Message.ToString(), Status = false };
+            }
+
+
+        }
+
+        public Result DeletePageContent(PAGE_CONTENT dataInput)
+        {
+            try
+            {
+                using (var context = new BIG_RY_DBEntities())
+                {
+                    context.PAGE_CONTENT.Attach(dataInput);
+                    context.PAGE_CONTENT.Remove(dataInput);
+                    context.SaveChanges();
+
+                    return new Result() { Message = "Success", Status = true };
+
+                }
+
+            }
+            catch (Exception ex)
+            {
+                return new Result() { Message = ex.Message.ToString(), Status = false };
+            }
+
+
+
+        }
+
+
+      
     }
 }
--- BIG_Web_Rayong/Controllers/AdminController.cs
+++ BIG_Web_Rayong/Controllers/AdminController.cs
@@ -1,8 +1,12 @@
-﻿using System;
+﻿using BIG.WEB.RY.DATASERVICE;
+using BIG.WEB.RY.MODEL;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Script.Serialization;
 
 namespace BIG_Web_Rayong.Controllers
 {
@@ -12,6 +16,359 @@ namespace BIG_Web_Rayong.Controllers
         public ActionResult Index()
         {
             return View();
+        }
+
+
+        public ActionResult HomeManagement()
+        {
+            PageContent_Services service = new PageContent_Services();
+            var data = service.GetAll().ToList();
+
+            Content dataContent = new Content()
+            {
+                AboutUs = data.Where(x => x.SECTION_NAME == "About Us").ToList(),
+                SlideImage = data.Where(x => x.SECTION_NAME == "Slide Images").ToList(),
+                Branches = data.Where(x => x.SECTION_NAME == "Branches").ToList(),
+                CEO = data.Where(x => x.SECTION_NAME == "CEO").ToList(),
+                Mission = data.Where(x => x.SECTION_NAME == "Mission").ToList(),
+                Vision = data.Where(x => x.SECTION_NAME == "Vision").ToList(),
+                Policy = data.Where(x => x.SECTION_NAME == "Policy").ToList()
+
+
+            };
+
+
+
+            return View(dataContent);
+        }
+
+        public ActionResult CustomerManagement()
+        {
+            PageContent_Services service = new PageContent_Services();
+            Customer_Services serviceCust = new Customer_Services();
+            var data = service.GetAll().ToList();
+            var listCust = serviceCust.GetAll();
+
+            Content dataContent = new Content()
+            {
+                Customers = data.Where(x => x.SECTION_NAME == "Customer").ToList(),
+                CustomerList = listCust
+            };
+            return View(dataContent);
+
+
+
+        
+        }
+
+        public ActionResult QualityManagement()
+        {
+            PageContent_Services service = new PageContent_Services();
+            var data = service.GetAll().ToList();
+
+            Content dataContent = new Content()
+            {
+                Quality = data.Where(x => x.SECTION_NAME == "Quality").ToList(),
+             
+
+            };
+            return View(dataContent);
+
+        }
+
+        public ActionResult NewsManagement()
+        {
+            PageContent_Services service = new PageContent_Services();
+            var data = service.GetAll().ToList();
+
+            Content dataContent = new Content()
+            {
+                News = data.Where(x => x.SECTION_NAME == "News").ToList(),
+
+
+            };
+            return View(dataContent);
+
+        }
+        public ActionResult SaveDataImageSlide(Content dataInput, string type)
+        {
+            type = dataInput.PageContent.AUTO_ID > 0 ? "Edit" : "Add";
+            dataInput.PageContent.SECTION_NAME = "Slide Images";
+            dataInput.PageContent.TYPE_ID = 1;
+            if(dataInput.PageContentUpload != null)
+            {
+                var file = dataInput.PageContentUpload;
+                var fileName = Path.GetFileName(file.FileName);
+                var files = Directory.GetFiles(Server.MapPath("~/Content/Banner"));
+
+                if (!Directory.Exists(Server.MapPath("~/Content/Banner")))
+                {
+                    Directory.CreateDirectory(Server.MapPath("~/Content/Banner"));
+                }
+                var exist = (from p in files
+                             where p.Contains(fileName)
+                             select p).ToList();
+                if (exist.Count > 0)
+                {
+                    fileName = Path.GetFileNameWithoutExtension(fileName) + "_" + exist.Count + exist.Count + Path.GetExtension(fileName);
+                }
+                if (file != null && file.ContentLength > 0)
+                {
+                    var path = Path.Combine(Server.MapPath("~/Content/Banner"), fileName);
+                    file.SaveAs(path);
+                }
+                dataInput.PageContent.IMAGE_URL = "/Content/Banner/" + fileName;
+            }
+            Result resultTrans;
+            if(type == "Add")
+            {
+                resultTrans = new PageContent_Services().AddPageContent(dataInput.PageContent);
+            }
+            else
+            {
+                resultTrans = new PageContent_Services().EditPageContent(dataInput.PageContent);
+            }
+            return resultTrans.Status == true ? RedirectToAction("HomeManagement", "Admin") : null;
+        }
+
+        public ActionResult SaveDataAboutUs(Content dataInput)
+        {
+      
+            dataInput.PageContent.SECTION_NAME = "About Us";
+            dataInput.PageContent.TYPE_ID = 1;
+            if (dataInput.PageContentUpload != null)
+            {
+                var file = dataInput.PageContentUpload;
+                var fileName = Path.GetFileName(file.FileName);
+                var files = Directory.GetFiles(Server.MapPath("~/big_img"));
+
+                if (!Directory.Exists(Server.MapPath("~/big_img")))
+                {
+                    Directory.CreateDirectory(Server.MapPath("~/big_img"));
+                }
+                var exist = (from p in files
+                             where p.Contains(fileName)
+                             select p).ToList();
+                if (exist.Count > 0)
+                {
+                    fileName = Path.GetFileNameWithoutExtension(fileName) + "_" + exist.Count + exist.Count + Path.GetExtension(fileName);
+                }
+                if (file != null && file.ContentLength > 0)
+                {
+                    var path = Path.Combine(Server.MapPath("~/big_img"), fileName);
+                    file.SaveAs(path);
+                }
+                dataInput.PageContent.IMAGE_URL = "/big_img/" + fileName;
+            }
+            Result resultTrans;
+          
+                resultTrans = new PageContent_Services().EditPageContent(dataInput.PageContent);
+            return resultTrans.Status == true ? RedirectToAction("HomeManagement", "Admin") : null;
+        }
+
+        public ActionResult SaveDataQuality(Content dataInput)
+        {
+
+            dataInput.PageContent.SECTION_NAME = "Quality";
+            dataInput.PageContent.TYPE_ID = 4;
+            if (dataInput.PageContentUpload != null)
+            {
+                var file = dataInput.PageContentUpload;
+                var fileName = Path.GetFileName(file.FileName);
+                var files = Directory.GetFiles(Server.MapPath("~/big_img"));
+
+                if (!Directory.Exists(Server.MapPath("~/big_img")))
+                {
+                    Directory.CreateDirectory(Server.MapPath("~/big_img"));
+                }
+                var exist = (from p in files
+                             where p.Contains(fileName)
+                             select p).ToList();
+                if (exist.Count > 0)
+                {
+                    fileName = Path.GetFileNameWithoutExtension(fileName) + "_" + exist.Count + exist.Count + Path.GetExtension(fileName);
+                }
+                if (file != null && file.ContentLength > 0)
+                {
+                    var path = Path.Combine(Server.MapPath("~/big_img"), fileName);
+                    file.SaveAs(path);
+                }
+                dataInput.PageContent.IMAGE_URL = "/big_img/" + fileName;
+            }
+            Result resultTrans;
+
+            resultTrans = new PageContent_Services().EditPageContent(dataInput.PageContent);
+            return resultTrans.Status == true ? RedirectToAction("QualityManagement", "Admin") : null;
+        }
+
+        public ActionResult SaveDataNews(Content dataInput ,string type)
+        {
+            type = dataInput.PageContent.AUTO_ID > 0 ? "Edit" : "Add";
+            dataInput.PageContent.SECTION_NAME = "News";
+            dataInput.PageContent.TYPE_ID = 6;
+            if (dataInput.PageContentUpload != null)
+            {
+                var file = dataInput.PageContentUpload;
+                var fileName = Path.GetFileName(file.FileName);
+                var files = Directory.GetFiles(Server.MapPath("~/CSR"));
+
+                if (!Directory.Exists(Server.MapPath("~/CSR")))
+                {
+                    Directory.CreateDirectory(Server.MapPath("~/CSR"));
+                }
+                var exist = (from p in files
+                             where p.Contains(fileName)
+                             select p).ToList();
+                if (exist.Count > 0)
+                {
+                    fileName = Path.GetFileNameWithoutExtension(fileName) + "_" + exist.Count + exist.Count + Path.GetExtension(fileName);
+                }
+                if (file != null && file.ContentLength > 0)
+                {
+                    var path = Path.Combine(Server.MapPath("~/CSR"), fileName);
+                    file.SaveAs(path);
+                }
+                dataInput.PageContent.IMAGE_URL = "/CSR/" + fileName;
+            }
+            Result resultTrans;
+            if (type == "Add")
+            {
+                resultTrans = new PageContent_Services().AddPageContent(dataInput.PageContent);
+            }
+            else
+            {
+                resultTrans = new PageContent_Services().EditPageContent(dataInput.PageContent);
+            }
+            return resultTrans.Status == true ? RedirectToAction("NewsManagement", "Admin") : null;
+        }
+
+        public ActionResult SaveDataCEO(Content dataInput)
+        {
+
+            dataInput.PageContent.SECTION_NAME = "CEO";
+            dataInput.PageContent.TYPE_ID = 1;
+            if (dataInput.PageContentUpload != null)
+            {
+                var file = dataInput.PageContentUpload;
+                var fileName = Path.GetFileName(file.FileName);
+                var files = Directory.GetFiles(Server.MapPath("~/big_img"));
+
+                if (!Directory.Exists(Server.MapPath("~/big_img")))
+                {
+                    Directory.CreateDirectory(Server.MapPath("~/big_img"));
+                }
+                var exist = (from p in files
+                             where p.Contains(fileName)
+                             select p).ToList();
+                if (exist.Count > 0)
+                {
+                    fileName = Path.GetFileNameWithoutExtension(fileName) + "_" + exist.Count + exist.Count + Path.GetExtension(fileName);
+                }
+                if (file != null && file.ContentLength > 0)
+                {
+                    var path = Path.Combine(Server.MapPath("~/big_img"), fileName);
+                    file.SaveAs(path);
+                }
+                dataInput.PageContent.IMAGE_URL = "/big_img/" + fileName;
+            }
+            Result resultTrans;
+
+            resultTrans = new PageContent_Services().EditPageContent(dataInput.PageContent);
+            return resultTrans.Status == true ? RedirectToAction("HomeManagement", "Admin") : null;
+        }
+
+        public ActionResult SaveDataBranches(Content dataInput)
+        {
+
+            dataInput.PageContent.SECTION_NAME = "Branches";
+            dataInput.PageContent.TYPE_ID = 1;
+            Result resultTrans;
+            resultTrans = new PageContent_Services().EditPageContent(dataInput.PageContent);
+            return resultTrans.Status == true ? RedirectToAction("HomeManagement", "Admin") : null;
+        }
+        public ActionResult DeletePageContent(int id)
+        {
+            PAGE_CONTENT dataInput = new PAGE_CONTENT()
+            {
+                AUTO_ID = id
+
+            };
+            PageContent_Services service = new PageContent_Services();
+            var data = service.GetAll().ToList();
+            int? section = data.Where(x => x.AUTO_ID == id).First().TYPE_ID;
+            string controller = "";
+            switch (section)
+            {
+                case 1:
+                    controller = "HomeManagement";
+                    break;
+                case 2:
+                    controller = "ServiceManagement";
+                    break;
+                case 4:
+                    controller = "QualityManagement";
+                    break;
+                case 6:
+                    controller = "NewsManagement";
+                    break;
+                default:
+                    controller = "HomeManagement";
+                    break;
+            }
+
+            var resultTrans = new PageContent_Services().DeletePageContent(dataInput);
+          
+           return resultTrans.Status == true ? RedirectToAction(controller, "Admin") : null;
+        }
+
+        public ActionResult SaveDataCustomer(Content dataInput, string type)
+        {
+            type = dataInput.PageCustomer.AUTO_ID > 0 ? "Edit" : "Add";
+            if (dataInput.PageCustomerUpload != null)
+            {
+                var file = dataInput.PageCustomerUpload;
+                var fileName = Path.GetFileName(file.FileName);
+                var files = Directory.GetFiles(Server.MapPath("~/customer_img"));
+
+                if (!Directory.Exists(Server.MapPath("~/customer_img")))
+                {
+                    Directory.CreateDirectory(Server.MapPath("~/customer_img"));
+                }
+                var exist = (from p in files
+                             where p.Contains(fileName)
+                             select p).ToList();
+                if (exist.Count > 0)
+                {
+                    fileName = Path.GetFileNameWithoutExtension(fileName) + "_" + exist.Count + exist.Count + Path.GetExtension(fileName);
+                }
+                if (file != null && file.ContentLength > 0)
+                {
+                    var path = Path.Combine(Server.MapPath("~/customer_img"), fileName);
+                    file.SaveAs(path);
+                }
+                dataInput.PageCustomer.LOGO_URL = "/customer_img/" + fileName;
+            }
+            Result resultTrans;
+            if (type == "Add")
+            {
+                resultTrans = new Customer_Services().AddPageCustomer(dataInput.PageCustomer);
+            }
+            else
+            {
+                resultTrans = new Customer_Services().EditPageCustomer(dataInput.PageCustomer);
+            }
+            return resultTrans.Status == true ? RedirectToAction("CustomerManagement", "Admin") : null;
+        }
+
+        public ActionResult DeletePageCustomer(int id)
+        {
+            PAGE_CUSTOMER dataInput = new PAGE_CUSTOMER()
+            {
+                AUTO_ID = id
+
+            };
+            var resultTrans = new Customer_Services().DeletePageCustomer(dataInput);
+            return resultTrans.Status == true ? RedirectToAction("CustomerManagement", "Admin") : null;
         }
     }
 }
